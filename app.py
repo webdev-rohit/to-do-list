8d8f3a8b70179d67e0bf085c20aa7313cc133571
rohit14101998 <rohit14101998@outlook.com>
2022-11-23 19:15:31 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,56 @@
+# video - https://www.youtube.com/watch?v=oA8brF3w5XQ&t=2513s
+
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} {self.title}"
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        # print(title, description)
+        todo = Todo(title=title, description=description)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    # print(allTodo)
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route('/update/<int:sno>', methods=['GET','POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.description = description
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+if __name__=="__main__":
+    app.run(debug=True)
